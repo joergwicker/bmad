9942ab8b8e3061b7c16039a0fb8cefa87306dfcb
Joerg Wicker <wicker@uni-mainz.de>
2016-04-04 14:41:22 +1200

    updated readSparseWithMissing

--- src/main/java/org/kramerlab/bmad/general/ReadMatrix.java
+++ src/main/java/org/kramerlab/bmad/general/ReadMatrix.java
@@ -72,6 +72,11 @@ public class ReadMatrix{
 	String line = bur.readLine();
 	HashMap<Integer, Integer> mappingPos = new HashMap<Integer, Integer>();
 	HashMap<Integer, Integer> mappingNeg = new HashMap<Integer, Integer>();
+
+
+	HashMap<String, Integer> xMap = new HashMap<String, Integer>();
+	HashMap<String, Integer> yMap = new HashMap<String, Integer>();
+
 	
 	int sizeX = 0;
 	int sizeY = 0;
@@ -79,28 +84,35 @@ public class ReadMatrix{
 	while (line != null) {
 	    String[] linespl = line.split(" ");
 	    
-	    int x = Integer.parseInt(linespl[0]);
-	    int y = Integer.parseInt(linespl[1]);
+	    String x = linespl[0];
+	    String y = linespl[1];
+
+
+	    
+	    if(xMap.get(linespl[0]) == null){
+		xMap.put(linespl[0], sizeX);
+		sizeX++;
+	    }
+
+	    if(yMap.get(linespl[1]) == null){
+		yMap.put(linespl[1], sizeY);
+		sizeY++;
+		
+	    }
+	    
 	    if (linespl[2].equals("t")) {
-		mappingPos.put(x,y);
+		mappingPos.put(xMap.get(linespl[0]),
+			       xMap.get(linespl[1]));
 
 	    }
 	    if (linespl[2].equals("n")) {
-		mappingNeg.put(x,y);
+		mappingNeg.put(xMap.get(linespl[0]),
+			       xMap.get(linespl[1]));
 
-	    }
-
-
-
-	    if (x > sizeX) {
-		sizeX = x;
-	    }
-
-	    if (y > sizeY) {
-		sizeY = y;
 	    }
 
 	    
+
 	    line = bur.readLine();
 	}
 
